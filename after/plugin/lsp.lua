acd17edaf9b7b4263e5c83db961ffdc57272deaa
ujjwal sharma <74070381+ujjwalshrma@users.noreply.github.com>
2024-08-11 00:38:46 +0530

    Update lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,8 +1,7 @@
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('user_lsp_attach', {clear = true}),
+  group = vim.api.nvim_create_augroup('user_lsp_attach', { clear = true }),
   callback = function(event)
-    local opts = {buffer = event.buf}
-
+    local opts = { buffer = event.buf }
     vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -20,7 +19,7 @@ local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer'},
+  ensure_installed = { 'tsserver', 'rust_analyzer' },
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({
@@ -36,7 +35,7 @@ require('mason-lspconfig').setup({
               version = 'LuaJIT'
             },
             diagnostics = {
-              globals = {'vim'},
+              globals = { 'vim' },
             },
             workspace = {
               library = {
@@ -51,25 +50,25 @@ require('mason-lspconfig').setup({
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 -- this is the function that loads the extra snippets to luasnip
 -- from rafamadriz/friendly-snippets
 require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'luasnip', keyword_length = 2},
-    {name = 'buffer', keyword_length = 3},
-  },
   mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<cr>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
   }),
+  sources = {
+    { name = 'path' },
+    { name = 'nvim_lsp' },
+    { name = 'luasnip', keyword_length = 2 },
+    { name = 'buffer',  keyword_length = 3 },
+  },
   snippet = {
     expand = function(args)
       require('luasnip').lsp_expand(args.body)
